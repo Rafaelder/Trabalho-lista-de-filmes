fc7b3ebc5bc76af5dc8c36cbbc4dfe482c7497e1
Rafaelrib <elderrafael6s8p@gmail.com>
2024-09-24 21:48:36 -0300

    0.5

--- Filmes.js
+++ Filmes.js
@@ -2,7 +2,7 @@ function comecar(){                                                        //Só
 
 
     let listaAssistidos = [], listaParaAssistir = []                       // 2 arrays para armazenar os filmes; Funcionam como 2 listas de filmes 
-    let filme = null, sair = 0, opcao=-1
+    let filme = null, sair = 0, opcao=-1, i
 
     /*Função para pesquisar um filme específico em algum dos arrays; Pode ter a opção de selecionar o array 
     em que se quer fazer a pesquisa e depois se o usuario quer pesquisar por titulo, genero ou ano de lançamento*/
@@ -12,24 +12,38 @@ function comecar(){                                                        //Só
     
     /*Função para adicionar novos filmes, com seu titulo, genero e/ou ano; pode dar a opção de escolher em qual
     das listas/arrays voce quer adicionar o filme e deve adicionar-los no array em ordem do mais novo para o mais antigo*/
+    function escolherLista(){
+        console.log("\n1 - Filmes para assistir")
+        console.log("2 - Filmes assistidos\n")
+        return prompt("Digite o número da lista: ")
+    }
+
     function adicionarFilme(){  
-        console.log("\n -----------------Adicionando-filme----------------------\n")                                          
+        console.log("\n -----------------Adicionando-filme-------------------------\n")                                          
         let titulo = prompt("Digite o título do filme: ")
         let genero = prompt("Gênero: ")
         let ano = prompt("Ano de lançamento: ")
         filme = new Filme(titulo, genero, ano)
-        console.log("\n1 - Filmes para assistir")
-        console.log("2 - Filmes assistidos\n")
-        opcao = prompt("Em qual lista deseja adicionar o filme? ")
-        if(opcao == 1){                                                                 // talvez tenha que tratar para que adicione direto no 0 caso o array esteja vazio
-            for(let i = listaParaAssistir.length; i > 0; i--){
-                listaParaAssistir[i] = listaParaAssistir[i-1];
-            } listaParaAssistir[0] = filme
+        console.log(filme)
+        console.log("\n--- Adicionar filme em: ")
+        opcao = escolherLista()
+        if(opcao == 1){ 
+            if(listaParaAssistir.length == 0){
+                listaParaAssistir[0] = filme
+            } else{
+                for(i = listaParaAssistir.length; i > 0; i--){
+                    listaParaAssistir[i] = listaParaAssistir[i-1];
+                } listaParaAssistir[0] = filme
+            }                                                                        
             console.log("\n--------Filme adicionado na lista de filmes para Assistir--------\n")
         }else if(opcao == 2){
-            for(let i = listaAssistidos.length; i > 0; i--){
-                listaAssistidos[i] = listaAssistidos[i-1];
-            } listaAssistidos[0] = filme
+            if(listaAssistidos.length == 0){
+                listaAssistidos[0] = filme
+            } else{
+                for(i = listaAssistidos.length; i > 0; i--){
+                    listaAssistidos[i] = listaAssistidos[i-1];
+                } listaAssistidos[0] = filme
+            }         
             console.log("\n--------Filme adicionado na lista de filmes Assistidos--------\n")
         }
     
@@ -47,8 +61,28 @@ function comecar(){                                                        //Só
     }
 
     // apenas exibe os filme de alguma das lista; opção de escolher qual lista mostrar
-    function exibirFilmes(){
-
+    function exibirLista(){
+        console.log("\n--- Lista a ser exibida: ")
+        opcao = escolherLista()
+        if(opcao == 1){
+            if(listaParaAssistir.length == 0){
+                console.log("\n-----------------A lista ainda está vazia------------------\n")
+            }else{
+                console.log("\n\nLista de filmes para assistir: \n")
+                for(i = 0; i < listaParaAssistir.length; i++){
+                    console.log(`-> ${listaParaAssistir[i].titulo}  Gênero: ${listaParaAssistir[i].genero}  Ano de Lançamento: ${listaParaAssistir[i].ano} \n`)
+                }  
+            }           
+        }else if(opcao == 2){
+            if(listaAssistidos.length == 0){
+                console.log("\n-----------------A lista ainda está vazia------------------\n")
+            } else{
+                console.log("\n\nLista de filmes assistidos: \n")
+                for(i = 0; i < listaAssistidos.length; i++){
+                    console.log(`-> ${listaAssistidos[i].titulo}  Gênero: ${listaAssistidos[i].genero}  Ano de Lançamento: ${listaAssistidos[i].ano} \n`)
+                } 
+            }    
+        }      
     }
     
     class Filme{                                                           
@@ -65,23 +99,21 @@ function comecar(){                                                        //Só
         console.log("   2. Novo Filme \n")
         console.log("   3. Alterar Filme \n")
         console.log("   4. Deletar Filme \n")
-        console.log("   5. Mostrar todos os Filmes \n")
+        console.log("   5. Exibir lista\n")
         console.log("   6. Sair \n")
         opcao = parseInt(prompt("Digite um número: "))                    //Seleção de uma das opções do menu
-        if(opcao == 6)
+        if(opcao == 6 )
             sair = 1
         else if(opcao == 5){
-
-        }
-        else if(opcao == 4){
-
-        }
-        else if(opcao == 3){
-
+            exibirLista()
+        }else if(opcao == 4){
+            deletarFilme()
+        }else if(opcao == 3){
+            alterarFilme()
         }else if(opcao == 2){
             adicionarFilme()
         }else if(opcao == 1){
-
+            pesquisarFilme()
         }else
             alert("\n -----------------! Opção invalida! ------------------\n")
     } while (sair == 0)
